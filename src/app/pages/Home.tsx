7f8bdb594f06fdd97d1008df536a748932c71d39
Babatunde Akerele <babsake@gmail.com>
2023-04-20 11:41:36 +0100

    setup svgr for smooth svg imports

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react';
+import { MoneyBagIcon } from 'app/assets/icons';
 
 const Home: FC = () => {
   return (
     <>
       <div>Home page</div>
       <h1>Google Font</h1>
+      <MoneyBagIcon />
     </>
   );
 };
