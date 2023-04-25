1ae63a9981e8e7124d4c7aad9faf03238ff3798a
Abiodun Segun <abiodun_segun10@yahoo.com>
2023-04-25 11:16:24 +0100

    landing-page/sign-up-banner

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -4,6 +4,7 @@ import { Box } from '@chakra-ui/react';
 import Intro from 'app/components/landing-page/Intro';
 import CreativeCommunity from 'app/components/landing-page/CreativeCommunity';
 import HowItWork from 'app/components/landing-page/HowItWork';
+import SignUpBanner from 'app/components/landing-page/SignUpBanner';
 
 const Home: FC = () => {
   return (
@@ -11,6 +12,7 @@ const Home: FC = () => {
       <Intro />
       <CreativeCommunity />
       <HowItWork />
+      <SignUpBanner />
     </Box>
   );
 };
