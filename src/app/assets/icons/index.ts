b8ffa76aaa11b1c30c95219c829d4eb5081b906d
Abiodun Segun <abiodun_segun10@yahoo.com>
2023-05-10 15:08:57 +0100

    adding dashboard custom-tab

--- src/app/assets/icons/index.ts
+++ src/app/assets/icons/index.ts
@@ -1,2 +1,5 @@
 export { ReactComponent as MoneyBagIcon } from './money-bag.svg';
 export { ReactComponent as PaletteIcon } from './palette.svg';
+export { ReactComponent as DashboardIcon } from './dashboard.svg';
+export { ReactComponent as InvestmentIcon } from './investment.svg';
+export { ReactComponent as TestimonialIcon } from './testimonial.svg';
