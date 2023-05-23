c5d343bb44bee64e9bf4dfeea0d5da539f7d7f6e
Abiodun Segun <abiodun_segun10@yahoo.com>
2023-05-23 18:17:02 +0100

    remove react-fast-marquee

--- src/app/components/landing-page/CreativeCommunity.tsx
+++ src/app/components/landing-page/CreativeCommunity.tsx
@@ -1,9 +1,9 @@
 import { Box, Flex, Heading, Stack } from '@chakra-ui/react';
 // import creativeBox from '../../assets/images/home/growing-creativeBg.png';
-import Marquee from 'react-fast-marquee';
+// import Marquee from 'react-fast-marquee';
 import React, { FC } from 'react';
-import MarqueBtn from './MarqueBtn';
-import { CategoryData } from '../data/Category';
+// import MarqueBtn from './MarqueBtn';
+// import { CategoryData } from '../data/Category';
 
 const CreativeCommunity: FC = () => {
   return (
@@ -19,7 +19,7 @@ const CreativeCommunity: FC = () => {
           Our growing creative community
         </Heading>
 
-        <Stack spacing="30px">
+        {/* <Stack spacing="30px">
           <Marquee pauseOnHover speed={10} autoFill={true}>
             {CategoryData.slice(0, 10).map((item, index) => (
               <Box key={index} m="0 2.5rem">
@@ -42,7 +42,7 @@ const CreativeCommunity: FC = () => {
               </Box>
             ))}
           </Marquee>
-        </Stack>
+        </Stack> */}
         {/* <Box bgImg={creativeBox} w="full" h="279px"></Box> */}
       </Flex>
     </Box>
