5f8ca87b1dee62aaa0315470b98bd3677dda6aa7
Fortune Ellah <ellfortune3@gmail.com>
2023-06-13 14:29:31 +0100

    Creating and Routing the teams page

---
+++ src/app/pages/Teams.tsx
@@ -0,0 +1,16 @@
+import { Box } from '@chakra-ui/react'
+import { Footer } from 'app/layout/Footer'
+import Header from 'app/layout/Header'
+import React, { FC } from 'react'
+
+export const Teams: FC = () => {
+  return (
+    <Box>
+        <Header />
+        
+        <Footer />
+    </Box>
+  );
+};
+
+export default Teams;
