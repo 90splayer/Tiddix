4257f7a97e0af0e95534086e791e4f5d994620c4
Abiodun Segun <abiodun_segun10@yahoo.com>
2023-06-20 07:37:17 +0100

    social media profile

--- src/app/pages/dashboard/Editproject.tsx
+++ src/app/pages/dashboard/Editproject.tsx
@@ -5,14 +5,16 @@ import { Footer } from 'app/layout/Footer';
 import General from 'app/components/dashboard/edit profile/General';
 import EditProfile from 'app/components/dashboard/edit profile/EditProfile';
 import ProfilePassword from 'app/components/dashboard/edit profile/ProfilePassword';
+import SocialMedia from 'app/components/dashboard/edit profile/SocialMedia';
 
 const ProfileEdit = () => {
   return (
     <Box>
       <Header />
-      <General />
+      {/* <General /> */}
       {/* <EditProfile /> */}
       {/* <ProfilePassword /> */}
+      <SocialMedia />
       <Footer />
     </Box>
   );
