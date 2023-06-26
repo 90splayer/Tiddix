397e0fd5ae638300735351f5ef1a8662621643b0
Abiodun Segun <abiodun_segun10@yahoo.com>
2023-06-26 08:00:56 +0100

    grammarly/corporate-profile

--- src/app/components/about-page/CorporateProfile.tsx
+++ src/app/components/about-page/CorporateProfile.tsx
@@ -47,53 +47,53 @@ const CorporateProfile = () => {
         </Box>
         <Flex flexDir="column" px="10.2rem">
           <Text mb="5rem" textAlign="justify">
-            We established in the United Kingdom in 2023, Tiddix is 100% British
-            owned and operated and our head office is in London.
+            In 2023, we started operations in the United Kingdom. Our head
+            office is in London, and Tiddix is entirely British-owned and
+            operated.
             <br />
             <br />
             We are proud to be the bridge upon which the recognition of the
-            financial power of young creative minds stand. Since our
-            establishment, we have made over 100 dreams come though and we are
-            proud to report that we provided the launching pad to over a dozen
-            very successful creatives careers thus far.
+            financial power of young creative minds stands. Since our
+            establishment, we have made over 100 dreams come true, and we are
+            proud to report that we have provided the launching pad to over a
+            dozen very successful creative careers thus far.
           </Text>
           <Stack spacing="3rem">
             <Heading as="h2">Status quo</Heading>
             <Text textAlign="justify">
-              In the creative economy today, the nature of centralised markets
+              In the creative economy today, the nature of centralized markets
               means that creatives can only raise funds for production through
-              corporations like record labels and publishers often wrapping the
-              artist into long-term unfavourable deals that can derail both
+              corporations like record labels and publishers, often wrapping the
+              artist into long-term unfavorable deals that can derail both
               creativity and longevity.
               <br />
               <br />
               This deadlock between creatives and corporations has been
               challenged via crowdfunding platforms like Kickstarter. But rather
               than creating opportunities for investment with valuable returns,
-              these platforms often come with a “charity” spin making the
+              these platforms often come with a “charity” spin, making the
               “supporters” just donors happy to receive a stipulated reward. The
-              result of this is a fan-only supporting model cutting out
+              result of this is a fan-only supporting model that cuts out
               investors that might be interested in funding for financial
               return.
             </Text>
             <Heading as="h2">The disruption</Heading>
             <Text textAlign="justify">
               Tiddix aims to take advantage of the shift towards
-              decentralisation in finance to invite investors to the creative
+              decentralization in finance to invite investors to the creative
               space where creator metrics (views, engagements, followers,
-              rankings etc) can be used to properly determine a creator’s value
-              and prospects for investments.
+              rankings, etc.) can be used to properly determine a creator’s
+              value and prospects for investments.
               <br />
               <br />
               We hope that this new pipeline to funding will provide creators
               with the freedom to put out their best work while also giving
-              investors an early stage entrance into both projects and the
-              associated creators. A single smartphone in itself is now a
-              powerful enough tool to launch a career in creativity. We are
-              building xxx (state all the technology). With the advancement of
-              blockchain technology and other forms of cross-border payments and
-              trustless systems with programable smart-contracts, we believe the
-              world is finally ready for this frontier of creativity.
+              investors an early-stage entrance into both projects and the
+              associated creators. A single smartphone is now a powerful enough
+              tool to launch a career in creativity. With our unique scoring
+              system, we provide a platform that takes advantage of the latest
+              technologies to launch exciting projects to be a part of and the
+              funding to make them a reality.
             </Text>
 
             <Text mb="5rem" fontWeight="700" size="body1" color="#fff">
@@ -103,10 +103,10 @@ const CorporateProfile = () => {
             <Text textAlign="justify">
               The trend already exists in the emergence of many successful
               independent artists across the creative field, but with investor
-              backing, we believe the next producer would not be needing an
+              backing, we believe the next producer would not need an
               established production studio to go to market. After all, with the
-              democratisation of technology, most of the creativity we
-              experience today are developed by a network of creators building
+              democratization of technology, most of the creativity we
+              experience today is developed by a network of creators building
               magic from the comfort of their homes.
               <br />
               <br />
