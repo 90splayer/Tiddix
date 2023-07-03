d194667bc4b6cb3fe9cfcf8706df40fb76d5d164
Abiodun Segun <abiodun_segun10@yahoo.com>
2023-07-03 08:58:53 +0100

    withdraw modal

--- src/app/components/dashboard/WalletView.tsx
+++ src/app/components/dashboard/WalletView.tsx
@@ -25,6 +25,7 @@ import {
   Th,
   Thead,
   Tr,
+  VStack,
   useDisclosure,
 } from '@chakra-ui/react';
 import { IoWallet } from 'react-icons/io5';
@@ -36,6 +37,7 @@ import { GoChevronDown } from 'react-icons/go';
 
 const WalletView: FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Box>
       <Container
@@ -98,13 +100,18 @@ const WalletView: FC = () => {
               Deposit
             </Button>
 
-            <Button variant="secondary" fontSize="1.6rem" size="md">
+            <Button
+              variant="secondary"
+              onClick={onOpen}
+              fontSize="1.6rem"
+              size="md"
+            >
               Withdraw
             </Button>
           </Flex>
         </Flex>
         {/* Modal COMPONENT */}
-        <Modal size="4xl" isOpen={isOpen} onClose={onClose}>
+        <Modal size="4xl" isOpen={isOpen} isCentered onClose={onClose}>
           <ModalOverlay />
           <ModalContent borderRadius="20px" pt="8rem" bg="#232629" pb="11.2rem">
             <ModalHeader pb="4.5rem">
@@ -167,7 +174,129 @@ const WalletView: FC = () => {
                 maxW="39rem"
                 w="100%"
                 fontSize="1.6rem"
-                onClick={onClose}
+              >
+                Make Payment
+              </Button>
+            </Flex>
+          </ModalContent>
+        </Modal>
+        {/* Withdraw modal */}
+        <Modal size="4xl" isOpen={isOpen} isCentered onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent borderRadius="20px" pt="6rem" bg="#232629" pb="9.2rem">
+            <ModalHeader pb="3.5rem">
+              <Flex align="center" justify="center">
+                <Text color="#fff" fontSize="24px" fontWeight="700">
+                  Withdraw
+                </Text>
+              </Flex>
+            </ModalHeader>
+            <ModalCloseButton
+              color="#000"
+              p="10px"
+              bg="white"
+              _hover={{ opacity: '0.5' }}
+              borderRadius="50px"
+              fontSize="lg"
+            />
+            <ModalBody pb="3rem">
+              <Flex align="center" mb="3rem" justify="center" gap="1.4rem">
+                <Flex
+                  border="1px solid #99A1AA"
+                  align="center"
+                  justify="center"
+                  borderRadius="100%"
+                  p="2rem"
+                >
+                  <Icon as={HiMinus} fontSize="1.6rem" color="#fff" />
+                </Flex>
+                <Box>
+                  <Input
+                    pos="relative"
+                    placeholder="Enter Amount"
+                    px="1.5rem"
+                    type="number"
+                    borderRadius="2rem"
+                    border="1px solid #99A1AA"
+                    maxW="26.3rem"
+                    h="6.3rem"
+                    _placeholder={{
+                      fontSize: '1.6rem',
+                      color: '#99A1AA',
+                    }}
+                  />
+                </Box>
+                <Flex
+                  border="1px solid #99A1AA"
+                  align="center"
+                  justify="center"
+                  borderRadius="100%"
+                  p="2rem"
+                >
+                  <Icon as={HiPlus} fontSize="1.6rem" color="#fff" />
+                </Flex>
+              </Flex>
+              <VStack spacing="1.9rem">
+                <Box>
+                  <Input
+                    pos="relative"
+                    placeholder="select bank"
+                    px="1.5rem"
+                    type="number"
+                    borderRadius="2rem"
+                    border="1px solid #99A1AA"
+                    w="41.1rem"
+                    // maxW="41.1rem"
+                    h="6.3rem"
+                    _placeholder={{
+                      fontSize: '1.6rem',
+                      color: '#99A1AA',
+                    }}
+                  />
+                </Box>
+                <Box>
+                  <Input
+                    pos="relative"
+                    placeholder="Account Number"
+                    px="1.5rem"
+                    type="number"
+                    borderRadius="2rem"
+                    border="1px solid #99A1AA"
+                    // maxW="41.1rem"
+                    w="41.1rem"
+                    h="6.3rem"
+                    _placeholder={{
+                      fontSize: '1.6rem',
+                      color: '#99A1AA',
+                    }}
+                  />
+                </Box>
+                <Box>
+                  <Input
+                    pos="relative"
+                    placeholder="Account Name"
+                    px="1.5rem"
+                    type="number"
+                    borderRadius="2rem"
+                    border="1px solid #99A1AA"
+                    w="41.1rem"
+                    // maxW="41.1rem"
+                    h="6.3rem"
+                    _placeholder={{
+                      fontSize: '1.6rem',
+                      color: '#99A1AA',
+                    }}
+                  />
+                </Box>
+              </VStack>
+            </ModalBody>
+
+            <Flex align="center" justify="center">
+              <Button
+                variant="multicolor"
+                maxW="39rem"
+                w="100%"
+                fontSize="1.6rem"
               >
                 Make Payment
               </Button>
