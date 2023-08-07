b508a206f2fab9faf95f87a2685e56f595774867
Abiodun Segun <abiodun_segun10@yahoo.com>
2023-08-07 12:11:18 +0100

    redesign project tab

--- src/app/components/dashboard/project/UserProjects.tsx
+++ src/app/components/dashboard/project/UserProjects.tsx
@@ -9,12 +9,20 @@ import {
   Heading,
   Stack,
   Button,
+  Card,
+  CardHeader,
+  Progress,
+  SimpleGrid,
 } from '@chakra-ui/react';
 import ProjectCard from 'app/components/ProjectCard';
 import { projectData } from 'app/components/data/ProjectData';
 import { verify } from 'app/assets/svgs/home';
 import useApiPrivate from 'app/hooks/useApiPrivate';
 import { chkToaster } from 'app/components/common/Toaster';
+import { debt } from 'app/assets/svgs/dashboard/dashboard';
+import { thousandsSeparators } from 'app/utils/helpers';
+import { progress } from 'framer-motion';
+import img from 'app/assets/images/contact.png';
 
 type projectT = {
   amount: number;
@@ -48,64 +56,126 @@ const UserProjects: FC = () => {
   }, []);
 
   return (
-    <Box w="100%" py="3rem" h="auto">
+    // <Box w="100%" py="3rem" h="auto">
+    //   {projects.map((project) => (
+    //     <Flex
+    //       align="center"
+    //       gap="3rem"
+    //       borderBottom="1px solid #485155"
+    //       py="2rem"
+    //     >
+    //       <Flex w="100%" justify="space-between" align="center">
+    //         <HStack spacing="19px">
+    //           <Box boxSize="10rem">
+    //             <Image
+    //               w="100%"
+    //               h="10rem"
+    //               borderRadius="2rem"
+    //               objectFit="cover"
+    //               src={project.coverArt}
+    //               alt={project.projectName}
+    //             />
+    //           </Box>
+    //           <Stack>
+    //             <HStack>
+    //               <Avatar
+    //                 boxSize="25px"
+    //                 name={project.creativeName}
+    //                 src={project.creativePicture}
+    //               />
+    //               <Text size="body2" color="#fff">
+    //                 {project.creativeName}
+    //               </Text>
+    //               <Box>{project.creativeVerified && verify}</Box>
+    //             </HStack>
+    //             <Heading size="h3">{project.projectName}</Heading>
+    //             <Text fontSize="1.4rem">{project.investmentType}</Text>
+    //           </Stack>
+    //         </HStack>
+    //         <Stack>
+    //           <Text size="body2">Funding Type</Text>
+    //           <Text color="#fff">{project.investmentType}</Text>
+    //         </Stack>
+    //         <Stack>
+    //           <Text size="body2">Period</Text>
+    //           <Text color="#fff">5 Month</Text>
+    //         </Stack>
+    //         <Box>
+    //           <Button
+    //             variant="multiradial"
+    //             fontSize="1.4rem"
+    //             size="sm"
+    //             isDisabled={project.progress !== 100}
+    //           >
+    //             Withdraw
+    //           </Button>
+    //         </Box>
+    //       </Flex>
+    //     </Flex>
+    //   ))}
+    <SimpleGrid w="100%" columns={3} spacingX="24px" spacingY="30px">
       {projects.map((project) => (
-        <Flex
-          align="center"
-          gap="3rem"
-          borderBottom="1px solid #485155"
-          py="2rem"
-        >
-          <Flex w="100%" justify="space-between" align="center">
-            <HStack spacing="19px">
-              <Box boxSize="10rem">
-                <Image
-                  w="100%"
-                  h="10rem"
-                  borderRadius="2rem"
-                  objectFit="cover"
-                  src={project.coverArt}
-                  alt={project.projectName}
-                />
-              </Box>
-              <Stack>
-                <HStack>
-                  <Avatar
-                    boxSize="25px"
-                    name={project.creativeName}
-                    src={project.creativePicture}
-                  />
-                  <Text size="body2" color="#fff">
-                    {project.creativeName}
-                  </Text>
-                  <Box>{project.creativeVerified && verify}</Box>
-                </HStack>
-                <Heading size="h3">{project.projectName}</Heading>
-                <Text fontSize="1.4rem">{project.investmentType}</Text>
-              </Stack>
-            </HStack>
-            <Stack>
-              <Text size="body2">Funding Type</Text>
-              <Text color="#fff">{project.investmentType}</Text>
+        <Card bgColor="#232629" borderRadius="30px" p="13px 14.5px 32px 13px">
+          <Box
+            borderRadius="20px"
+            bgImage={project.coverArt}
+            backgroundPosition="center"
+            bgSize="cover"
+            h="265px"
+            w="100%"
+            mb="2rem"
+            pos="relative"
+            pl="1.5rem"
+            pb="1.5rem"
+          >
+            <Flex pos="absolute" bottom={5} flexDir="column" justify="flex-end">
+              <Text fontWeight="700" mb="3px" color="#fff">
+                {project.projectName}
+              </Text>
+              <Text size="body2">{project.category}</Text>
+            </Flex>
+          </Box>
+          <Flex justify="space-between" align="center" color="#fff">
+            <Stack spacing="2rem">
+              <HStack spacing="1rem">
+                <Box>{debt}</Box>
+                <Heading fontSize="1.6rem">{project.investmentType}</Heading>
+              </HStack>
+
+              <Heading fontSize="2rem">
+                £ {thousandsSeparators(project.amount)}
+              </Heading>
             </Stack>
-            <Stack>
-              <Text size="body2">Period</Text>
-              <Text color="#fff">5 Month</Text>
+            <Stack spacing="2rem">
+              <HStack gap="1rem">
+                <Button
+                  variant="multiradial"
+                  fontSize="1.2rem"
+                  p="1.5rem 1rem"
+                  size="xs"
+                  isDisabled={project.progress !== 100}
+                >
+                  Withdraw
+                </Button>
+                <Text mb="15px" fontSize="1.6rem" textAlign="right">
+                  {project.progress}%
+                </Text>
+              </HStack>
+
+              <Progress
+                value={project.progress}
+                size="md"
+                colorScheme="transparent"
+                borderRadius="20px"
+                background="linear-gradient(transparent, transparent) padding-box, linear-gradient(235.92deg, #FFC227 -14.27%, #FF8CDF 50.09%, #4EFCF9 114.81%) border-box"
+                w="10rem"
+              />
             </Stack>
-            <Box>
-              <Button
-                variant="multiradial"
-                fontSize="1.4rem"
-                size="sm"
-                isDisabled={project.progress !== 100}
-              >
-                Withdraw
-              </Button>
-            </Box>
           </Flex>
-        </Flex>
+        </Card>
       ))}
-    </Box>
+    </SimpleGrid>
+    // </Box>
   );
 };
 
